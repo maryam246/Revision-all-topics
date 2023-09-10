c7efe26599eca26db767964959b7fed0ed3666ec
Maryam khanam <143519643+maryam246@users.noreply.github.com>
2023-09-10 08:29:05 -0700

    Update Revision topic-8.py

--- Revision topic-8.py
+++ Revision topic-8.py
@@ -280,3 +280,253 @@ try:
         print(f"{int(t)} X {i} = {int(t)*i}")
 except:
     print("invalid input")
+
+# example of global ,local scope
+a =22
+def num1():
+   b =67
+   print(a)
+   print(b)
+num1()
+print(a)
+
+#indentation.
+x =100
+if x>20:
+   print("False")
+else:
+   print("TRUE")
+
+#Assignment statements
+i = 2
+j = 4
+x = i+j*j-i+7
+print(x)
+
+#Chek validity of string
+import keyword
+t = "True"
+if keyword.iskeyword(t):
+   print(f"Found {t}")
+else:
+   print("not found")
+
+#Another example
+import keyword
+y = "list"
+if keyword.iskeyword(y):
+   print("yes we found")
+else:
+   print("no")
+
+# Assigning value in python and other language
+x = 100
+y = "maryam"
+#in c++,java
+#int x = 100
+
+#example print a new line
+print("I",end=" ")
+print("am",end=" ")
+print("a",end=" ")
+print("girl.")
+
+# Example of basic calculator:
+def sum():
+   i = int(input("Enter 1st number: "))
+   j = int(input("Enter 2st number: "))
+   return i+j
+
+def div():
+   i = int(input("Enter 1st number: "))
+   j = int(input("Enter 2st number: "))
+   return i/j
+
+def mul():
+   i = int(input("Enter 1st number: "))
+   j = int(input("Enter 2st number: "))
+   return i*j
+
+def sub():
+   i = int(input("Enter 1st number: "))
+   j = int(input("Enter 2st number: "))
+   return i-j
+
+print("Select--> 1(sum)/2(div)/3(mul)/4(sub)")
+a = int(input("Select: "))
+if a==1:
+   print(sum())
+elif a==2:
+   print(div())
+elif a==3:
+   print(mul())
+elif a==4:
+   print(sub())
+else:
+   print("Invalid selection!")
+
+# Example of taking multiple input from user:
+name = input("Enter your name: ")
+print(name)
+age = int(input("Enter your age: "))
+print(age)
+CGPA = float(input("Enter your CGPA: "))
+print(CGPA)
+
+#Example of Python Input Methods for Competitive Programming:
+# input method:
+from sys import stdin,stdout
+a = stdin.readline()
+print(a)
+
+arr = [int(x) for x in stdin.readline().split()]
+
+print(arr)
+# output by stdout
+n = stdout.write(str(sum()))
+print(n)
+
+#Example of print() function
+m = "ali"
+n = 22
+o = 33.33
+print("Data of m,n,o is: ",m,n,o)
+
+#Example of sep ()function:
+print("I love","Pakistan","hy","ali",sep=" ")
+
+#Example of OUTPUT formating
+fruit = "Apple"
+price = 200
+#output using %
+print("Fruit: %s price: %d" % (fruit,price)) # %s for str, %f for float,%d for int
+
+sallary = 20000.0
+name = "Qamar"
+print("sallary: %d name: %s" % (sallary,name))
+
+# using{} braces
+print("name:{} sallary:{}".format(name,sallary))
+print("fruit:{} price:{}".format(fruit,price))
+
+#Another example
+a =20
+b ='a'
+print("a:%d b:%s"%(a,b))
+print("a:{} b:{} ".format(a,b))
+
+
+#Example of dictonary
+dic = {"ali":1,"maryam":2,"kiran":3}
+print(dic)
+
+##Example of  Name(A special variable)in python:
+a= 2
+b = 5
+z = a*b
+print(z)
+print("i am a ",__name__)
+if __name__=='__main__':
+   print("hello")
+
+#Example of private variable
+class A():
+   def __init__(self, a):
+      self.__a = a
+
+   def show(self):
+      print(f"{self.__a}")
+
+
+a = A("I am a vowel")
+a.show()
+
+#Another example
+class Student():
+   def __init__(self, name, age):
+      self.__name = name
+      self.__age = age
+
+   def display_info(self):
+      print(f"My name is {self.__name}, and my age is {self.__age}")
+
+
+s = Student("maryam", 20)
+s.display_info()
+
+#Another example
+class Papers():
+   def __init__(self, p1, p2):
+      self.__p1 = p1
+      self.__p2 = p2
+
+   def print_papers(self):
+      print(f"MY 1st paper is {self.__p1}, and 2nd paper is {self.__p2}")
+
+
+p = Papers('math', 'computer')
+p.print_papers()
+
+#Another example
+class City_names():
+   def __init__(self, c1, c2):
+      self.__c1 = c1
+      self.__c2 = c2
+
+   def prnt(self):
+      print(f"The first country name is {self.__c1}, and another city name is {self.__c2}")
+
+
+c = City_names('Islamabad', 'Karachi')
+c.prnt()
+
+#Example of check equality
+def check_equality(a,b):
+   if a>b:
+      print("a is greater than b")
+   elif a!=b:
+      print("a is not equal to b")
+   else:
+      print("Condition falls")
+check_equality(2,5)
+
+#Example of maximum possible value of an integer on your system
+import sys
+m = sys.maxsize
+print("m")
+
+#Example of packing and unpacking:
+#Unpacking:
+def fun(a,b,c,d):
+   print(a,b,c,d)
+
+l = [1,2,3,4]
+fun(*l)
+
+#Another example
+a = [2,8]
+print(list(range(*a)))
+
+#Packing
+def num(*n):
+    print(n)
+
+num(2,3,45,5)
+
+#Another example
+def colour(*c):
+    print(c)
+
+colour('orange','green','blue')
+
+def A(*d):
+   print(d)
+A(2,3,4,2)
+
+#Example of Bytes object VS String in
+a = "Great"
+b = b"Great"
+print(a)
+print(b  )
+print(type(a))
+print(type(b))
